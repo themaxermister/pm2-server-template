521260137d3e7c2c0f6b161da75e261dab60b7a3
themaxermister <jobelle.leeernee@gmail.com>
2025-02-06 22:07:20 +0800

    update run.sh file location

--- programs/test/run.sh
+++ programs/test/run.sh
@@ -8,7 +8,7 @@ name=$1
 age=$2
 height=$3
 
-cd ./programs/test || { printf "Directory not found!"; exit 1; }
+cd /programs/test || { printf "Directory not found!"; exit 1; }
 output=$(python3 test.py $name $age $height 2>&1)
 exit_code=$?
 
